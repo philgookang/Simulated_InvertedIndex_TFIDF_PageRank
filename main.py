ac40c699cc8289ae01c5cc00352d2cc57190cf8d
Phil Goo Kang <philgookang@gmail.com>
2019-03-20 21:52:43 +0900

    Added input

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ from component import *
     -> create inverted index table
     ->
 '''
-Initalize()
+#Initalize()
+
+while True:
+    search_text = input("2018-22788> ")
+    search_word = search_text.split()
+
+    for word in search_word:
+        result = []
